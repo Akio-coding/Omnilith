6aed02c4016f2b379c074357095ea2fac84acd3a
Bent3st <Okaminosaigai@gmail.com>
2026-03-25 14:52:27 +0100

    tuto barre de vie

---
+++ Assets/lifebar/healthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class healthBar : MonoBehaviour
+{
+    public Slider slider; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int health)
+    { 
+        slider.value = health;
+    }
+}
